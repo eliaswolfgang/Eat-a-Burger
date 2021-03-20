73e31dd348e8f7a71088a65c09ada78645198138
Ian Von Fange <wolfgangvonfange@gmail.com>
2021-03-19 23:00:34 -0500

    Updated process.env variables to deploy to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,19 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
-// And import util to promisify the queries to use async/await syntax
-const util = require('util')
+var connection;
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  // If you see this, please give me feedback on the best way to secure this password! I've been searching npm packages but don't know which one is the most intuitive to use.
-  password: 'ivmysql123',
-  database: 'burgers_db',
-});
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    // If you see this, please give me feedback on the best way to secure this password! I've been searching npm packages but don't know which one is the most intuitive to use.
+    password: 'ivmysql123',
+    database: 'burgers_db',
+  });
+};
 // Connect and log to console
 connection.connect((err) => {
   if (err) {
@@ -20,8 +22,6 @@ connection.connect((err) => {
   }
   console.log(`connected as id ${connection.threadId}`);
 });
-
-connection.query = util.promisify(connection.query);
 
 // Export connection for ORM
 module.exports = connection;
